178d1efd5278cab449c6898b1c4423a50d203ba0
Sonira <sonira.shokohyan531@gmail.com>
2023-07-17 20:00:00 +0500

    new file added

---
+++ formatstring.py
@@ -0,0 +1,24 @@
+# year = 2026
+# totalStudents = 7000
+
+# finalResult = "in year 2020 number of students were 4000"
+
+
+
+
+# we are doing this to make our sentences dynamic
+
+year = 2050
+totalStudents = 10000
+
+finalResult = "in {} number of students were {}"
+print(finalResult.format(year, totalStudents))
+
+
+
+firstNumber = 3
+secondNumber = 10
+
+count = "from {} number up to {} are constant"
+
+print(count.format(firstNumber, secondNumber))
